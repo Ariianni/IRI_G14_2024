05c147acf276661a462b5f0afc2f419f0a44cf0d
oxcar30 <oscarperdomo896@gmail.com>
2024-11-13 22:56:42 -0300

    13/11
    corriendo check
    caminando check
    durmiendo check
    falta arritmia y fibrilacion

--- TP 2024 corazon/estados.py
+++ TP 2024 corazon/estados.py
@@ -2,6 +2,23 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 
+T = 120
+tiempo = np.linspace(
+    0, T, 1000
+)
+#---------Mascaras de volumen ventricular----------------
+mask1 = (tiempo <= (10))
+mask2 = ((tiempo > (10)) & (tiempo <= (50)))
+mask3 = ((tiempo > (50)) & (tiempo <= (90)))
+mask4 = ((tiempo > (90)) & (tiempo <= (110)))
+mask5 = ((tiempo > (110)) & (tiempo <= (120)))
+
+#--------Mascaras de presion aortica-------------------
+Pmask1 = (tiempo >= 0) & (tiempo <= 1.99)
+Pmask2 = (tiempo > 1.99) & (tiempo <= 27)
+Pmask3 = (tiempo > 27) & (tiempo <= 44.5)
+Pmask4 = (tiempo > 44.5) & (tiempo <= 120)
+
 def menu():
     a=0
     while(a!=9):
@@ -24,52 +41,47 @@ def menu():
             case 3: 
                 os.system('cls')
                 corriendo()
-
+            case 4:
+                os.system('cls')
+                arritmia()
+            case 5:
+                os.system('cls')
+                fibrilacion()
 
 def caminando():
     print("caminando")
-    T = 120
-    tiempo = np.linspace(
-        0, T, 1000
-    )
-    #---------Mascaras de volumen ventricular----------------
-    mask1 = (tiempo <= (10))#0.347 + 1.5
-    mask2 = ((tiempo > (10)) & (tiempo <= (50)))
-    mask3 = ((tiempo > (50)) & (tiempo <= (90)))
-    mask4 = ((tiempo > (90)) & (tiempo <= (110)))
-    mask5 = ((tiempo > (110)) & (tiempo <= (120)))
-
-    #--------Mascaras de presion aortica-------------------
-    Amask1 = (tiempo <= (21.5))
-    Amask2 = ((tiempo > (21.5)) & (tiempo <= (40)))
-    Amask3 = ((tiempo > (40)) & (tiempo <= (43.2)))
-    Amask4 = ((tiempo > (43.2)) & (tiempo <= (120)))
 
     #-------------Volumen ventricular---------------------------
+    VD=150
+    VS=80
     yV = np.zeros_like(tiempo)  # Crear un array de ceros del mismo tamaño que tiempo
-    yV = yV + mask1 * ((1 / 50000) * np.exp(tiempo - 0.03) + 100)
-    yV = yV + mask2 * ((-np.log10((tiempo-9)**20))+80)
-    yV = yV + mask3 * ((1*np.log10((tiempo -48)**20)+50))
-    yV = yV + mask4 * (13*np.sin(((1/10) * (tiempo))+3)+89)
-    yV = yV + mask5 * (-np.sin(((1 / 4) * (tiempo)-3))+ 101)
+    yV = yV + mask1 * (VD)#(1 / 50000) * np.exp(tiempo - 0.03) +
+    yV = yV + mask2 * (((VD*(np.exp(-tiempo+2.3))**(1/(10)))+VS))
+    yV = yV + mask3 * ((1*np.log10((tiempo -48)**20)+VS-5))
+    a=(1*np.log10((tiempo -48)**20)+VS-5)
+    yV = yV + mask4 * ((VD-a-6)*np.sin(((1/10) * (tiempo))+3.4)+a+6)
+    yV = yV + mask5 * (-np.sin(((1 / 4) * (tiempo)-3))+VD)
+
     yV_total = np.tile(yV, 3)
     tiempo_V_total = np.linspace(0, 3 * 120, 3 * len(tiempo))
     plt.subplot(2, 1, 1)
-    plt.xlabel("  sistole  diastole        sistole  diastole         sistole  diastole",loc='left')
+    #plt.xlabel("  sistole  diastole        sistole  diastole         sistole  diastole",loc='left')
     plt.ylabel("Volumen(ml)")
     plt.grid()
     plt.plot(tiempo_V_total, yV_total)
 
     #--------------presion aortica-------------------------
-    yA = np.zeros_like(tiempo)  # Crear un array de ceros del mismo tamaño que tiempo
-    yA = yA + Amask1*(80)
-    yA = yA + Amask2*(-40*np.sin(0.135*tiempo+0.3)+80)
-    yA = yA + Amask3*((tiempo-41.35)**(2)+100)
-    yA = yA + Amask4*(np.exp(-0.07*tiempo+6.16)+80)
-    yA_total = np.tile(yA, 3)
+    Ps = 120  
+    Pd = 80  
+    yP = np.zeros_like(tiempo)
+    yP += Pmask1 * (-((Ps - Pd) / 20) * np.sin(0.15 * tiempo) + Pd)
+    yP += Pmask2 * (-((1) / 8) * (tiempo - ((Ps - Pd) / 2))**2 + Ps)
+    yP += Pmask3 * ((1 / 11) * (tiempo - (Ps - Pd))**2 + Pd +19)
+    yP += Pmask4 * (np.exp(-0.189 * tiempo + 10) + Pd)
+    yA_total = np.tile(yP, 3)
     tiempo_A_total = np.linspace(0, 3 * 120, 3 * len(tiempo))
     plt.subplot(2, 1, 2)
-    plt.xlabel("  sistole  diastole        sistole  diastole         sistole  diastole",loc='left')
+    #plt.xlabel("  sistole  diastole        sistole  diastole         sistole  diastole",loc='left')
     plt.ylabel("Presión(mmHg)")
     plt.plot(tiempo_A_total, yA_total)
     plt.grid()
@@ -77,9 +89,90 @@ def caminando():
 
     plt.show()
 
-
 def durmiendo():
     print("durmiendo")
 
+    #-------------Volumen ventricular---------------------------
+    VDd=120
+    VSd=80
+    y3 = np.zeros_like(tiempo)
+    y3 = y3 + mask1 * (VDd)
+    y3 = y3 + mask2 * (((VDd*(np.exp(-tiempo-1))**(1/(10)))+VSd))
+    y3 = y3 + mask3 * ((1*np.log10((tiempo -48)**20)+VSd-5))
+    cesar=(1*np.log10((tiempo -48)**20)+VSd-5)
+    y3 = y3 + mask4 * ((VDd-cesar-6)*np.sin(((1/10) * (tiempo))+3.4)+cesar+6)
+    y3 = y3 + mask5 * (-np.sin(((1 / 4) * (tiempo)-3))+VDd)
+
+    yV_total = np.tile(y3, 3)
+    tiempo_V_total = np.linspace(0, 3 * 120, 3 * len(tiempo))
+    plt.subplot(2, 1, 1)
+    plt.ylabel("Volumen(ml)")
+    plt.grid()
+    plt.plot(tiempo_V_total, yV_total)
+
+    #--------------presion aortica-------------------------
+    Ps = 120  
+    Pd = 80  
+    yP = np.zeros_like(tiempo)
+    yP += Pmask1 * (-((Ps - Pd) / 20) * np.sin(0.15 * tiempo) + Pd)
+    yP += Pmask2 * (-((1) / 8) * (tiempo - ((Ps - Pd) / 2))**2 + Ps)
+    yP += Pmask3 * ((1 / 11) * (tiempo - (Ps - Pd))**2 + Pd +19)
+    yP += Pmask4 * (np.exp(-0.189 * tiempo + 10) + Pd)
+    yA_total = np.tile(0.75*yP, 3)
+    tiempo_A_total = np.linspace(0, 3 * 120, 3 * len(tiempo))
+    plt.subplot(2, 1, 2)
+    #plt.xlabel("  sistole  diastole        sistole  diastole         sistole  diastole",loc='left')
+    plt.ylabel("Presión(mmHg)")
+    plt.plot(tiempo_A_total, yA_total)
+    plt.grid()
+
+
+    plt.show()
+
 def corriendo():
     print("corriendo")
+    #-------------Volumen ventricular---------------------------
+    VDc=200
+    VSc=90
+    y2 = np.zeros_like(tiempo)
+    y2 = y2 + mask1 * (VDc)
+    y2 = y2 + mask2 * (((VDc*(np.exp(-tiempo+4))**(1/(10)))+VSc))
+    y2 = y2 + mask3 * ((1*np.log10((tiempo -48)**20)+VSc-5))
+    cesar=(1*np.log10((tiempo -48)**20)+VSc-5)
+    y2 = y2 + mask4 * ((VDc-cesar-6)*np.sin(((1/10) * (tiempo))+3.4)+cesar+6)
+    y2 = y2 + mask5 * (-np.sin(((1 / 4) * (tiempo)-3))+VDc)
+
+
+
+    yV_total = np.tile(y2, 3)
+    tiempo_V_total = np.linspace(0, 3 * 120, 3 * len(tiempo))
+    plt.subplot(2, 1, 1)
+    plt.ylabel("Volumen(ml)")
+    plt.grid()
+    plt.plot(tiempo_V_total, yV_total)
+
+    #--------------presion aortica-------------------------
+    Ps = 120  
+    Pd = 80  
+    yP = np.zeros_like(tiempo)
+    yP += Pmask1 * (-((Ps - Pd) / 20) * np.sin(0.15 * tiempo) + Pd)
+    yP += Pmask2 * (-((1) / 8) * (tiempo - ((Ps - Pd) / 2))**2 + Ps)
+    yP += Pmask3 * ((1 / 11) * (tiempo - (Ps - Pd))**2 + Pd +19)
+    yP += Pmask4 * (np.exp(-0.189 * tiempo + 10) + Pd)
+    yA_total = np.tile(1.17*yP, 3)
+    tiempo_A_total = np.linspace(0, 3 * 120, 3 * len(tiempo))
+    plt.subplot(2, 1, 2)
+    #plt.xlabel("  sistole  diastole        sistole  diastole         sistole  diastole",loc='left')
+    plt.ylabel("Presión(mmHg)")
+    plt.plot(tiempo_A_total, yA_total)
+    plt.grid()
+
+
+    plt.show()
+
+def arritmia():
+    print("arritmia")
+
+def fibrilacion():
+    print("fibrilacion")
+
